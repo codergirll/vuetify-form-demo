65423f1ecab31d2fd2636d1fdacb9fffeb764d85
Nate Rabins <nrabins@alkamitech.com>
2019-03-27 12:41:44 -0500

    Add flat demo project

--- src/App.vue
+++ src/App.vue
@@ -2,37 +2,29 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>Demo</span>
       </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
     </v-toolbar>
 
     <v-content>
-      <HelloWorld/>
+      <v-container>
+        <v-layout>
+          <v-flex>
+            <DessertList/>
+          </v-flex>
+        </v-layout>
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import DessertList from "@/components/DessertList";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  },
-  data () {
-    return {
-      //
-    }
+    DessertList
   }
-}
+};
 </script>
